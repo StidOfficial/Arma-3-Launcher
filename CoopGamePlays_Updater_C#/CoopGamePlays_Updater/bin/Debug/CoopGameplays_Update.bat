e0f83acbe921ccd9adb09f88bded7f536c02abbd
StidOfficial <StidOfficial@users.noreply.github.com>
2014-08-23 10:59:15 +0200

    Update CoopGameplays_Update.bat

--- CoopGamePlays_Updater_C#/CoopGamePlays_Updater/bin/Debug/CoopGameplays_Update.bat
+++ CoopGamePlays_Updater_C#/CoopGamePlays_Updater/bin/Debug/CoopGameplays_Update.bat
@@ -1,2 +1,13 @@
 @echo off
-CoopGamePlays_Updater.exe +mpm 102.102.102.102 -mpm
+REM Voci les options de lancement suivant :
+REM
+REM -nosplash Désactive l'introduction au lancement du jeu.
+REM -world=empty Au lancement du jeu, le jeu démarre dans un monde vide.
+REM -noPause Permet de ne pas avoir le jeu freezer quand on change de programme.
+REM -cpuCount=4 Attribuer un nombre de coeur au jeu en fonction de votre procésseur.
+REM -exThreads=7 Utilise tous les processus pour le chargement de la géométrie et des textures
+REM -maxMem=1024 Donner un nombre maximum de RAM en fonction de votre RAM. 
+REM -maxVram=3072 Donner un nombre maximum de memoire vidéo en fonction de la carte graphique.
+REM -window Lancer le jeu en fenêtre.
+
+CoopGamePlays_Updater.exe -option
